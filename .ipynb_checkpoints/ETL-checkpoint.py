1493cb3102e7b344be627ab89b782ad6b1e7f399
lucasguti98 <lucasguti.98@gmail.com>
2024-02-06 13:27:25 -0300

    first commit

---
+++ .ipynb_checkpoints/ETL-checkpoint.py
@@ -0,0 +1,105 @@
+import json
+import ast
+import pandas as pd
+import gzip
+import shutil
+
+# Descomprimo los archivos
+
+"""with gzip.open('user_reviews.json.gz', 'rb') as file_in:
+    with open('user_reviews.json', 'wb') as file_out:
+        shutil.copyfileobj(file_in, file_out)
+
+with gzip.open('steam_games.json.gz', 'rb') as file_in:
+    with open('steam_games.json', 'wb') as file_out:
+        shutil.copyfileobj(file_in, file_out)"""
+
+with gzip.open('users_items.json.gz', 'rb') as file_in:
+    with open('users_items.json', 'wb') as file_out:
+        shutil.copyfileobj(file_in, file_out)
+
+# Limpio los json y los convierto en csv
+
+"""fr=open("user_reviews.json", "r", encoding="utf-8")
+fw=open("user_reviews2.json", "w")
+
+for line in fr:
+    json_dat = json.dumps(ast.literal_eval(line))
+    dict_dat = json.loads(json_dat)
+    json.dump(dict_dat, fw)
+    fw.write("\n")
+
+fw.close()
+fr.close()
+        
+input_filename = "user_reviews2.json"
+output_filename = "user_reviews_cleaned.json"
+
+with open(input_filename, "r") as fr, open(output_filename, "w") as fw:
+    fw.write("[\n")
+    for line in fr:
+        line = line.strip()
+        if line.endswith(""):
+            fw.write(line + ",\n")
+        else:
+            fw.write(line + "\n")
+    fw.write("]\n")
+
+df_reviews = pd.read_json('user_reviews_cleaned.json')
+df_reviews.to_csv('user_reviews.csv')"""
+
+
+fr=open("users_items.json", "r", encoding="utf-8")
+fw=open("users_items2.json", "w")
+
+for line in fr:
+    json_dat = json.dumps(ast.literal_eval(line))
+    dict_dat = json.loads(json_dat)
+    json.dump(dict_dat, fw)
+    fw.write("\n")
+
+fw.close()
+fr.close
+
+input_filename = "users_items2.json"
+output_filename = "users_items_cleaned.json"
+
+with open(input_filename, "r") as fr, open(output_filename, "w") as fw:
+    fw.write("[\n")
+    for line in fr:
+        line = line.strip()
+        if line.endswith(","):
+            fw.write(line + "\n")
+        else:
+            fw.write(line + ",\n")
+    fw.write("]\n")
+
+"""df_items = pd.read_json("users_items_cleaned.json")
+df_items.to_csv("users_items.csv")
+"""
+
+"""input_filename = "steam_games.json"
+output_filename = "steam_games2.json"
+
+with open(input_filename, "r", encoding="utf-8") as fr, open(output_filename, "w") as fw:
+    for line in fr:
+        line = line.strip()
+        if line.endswith(","):
+            fw.write(line + "\n")
+        else:
+            fw.write(line + ",\n")
+
+input_file = "steam_games2.json"
+output_file = "steam_games3.json"
+
+with open(input_file, "r") as fr, open(output_file, "w") as fw:
+    json_data = fr.read()
+    cleaned_data = json_data.replace("NaN", "null")
+    fw.write(cleaned_data)
+
+fw.close()
+fr.close()
+
+df = pd.read_json("steam_games3.json")
+df.to_csv("steam_games.csv")
+"""
---
+++ main.py
@@ -0,0 +1,58 @@
+from fastapi import FastAPI
+import pandas as pd
+app = FastAPI()
+
+#Endpoint 1
+@app.get("/Endpoint 1/{developer}")
+def developer( developer : str ):   
+    df_endpoint1 = pd.read_csv("df_endpoint1.csv")
+    developer_data = df_endpoint1[df_endpoint1["developer"] == developer]
+    developer_data_dict = developer_data.to_dict(orient="records")
+
+    return developer_data_dict
+
+#Endpoint 2
+@app.get("/userdata/{user}")
+def userdata(user_id : str):   
+    df_endpoint2 = pd.read_csv("df_endpoint2.csv")
+    user_data = df_endpoint2[df_endpoint2["user_id"] == user_id]
+    user_data_dict = user_data.to_dict(orient="records")
+
+    return user_data_dict
+
+#Endpoint 3
+@app.get("/UserForGenre/{genre}")
+def  UserForGenre( genre : str ):   
+    df_endpoint3 = pd.read_csv("df_endpoint3.csv")
+    genre_data = df_endpoint3[df_endpoint3["genre"] == genre]
+    genre_data_dict = genre_data.to_dict(orient="records")
+
+    return genre_data_dict
+
+#Endpoint 4
+@app.get("/best_developer_year/{developer}")
+def best_developer_year( año : int ):
+    df_endpoint4 = pd.read_csv("df_endpoint4.csv")
+    developer_data = df_endpoint4[df_endpoint4["año"] == año]
+    developer_data_dict = developer_data.to_dict(orient="records")
+
+    return developer_data_dict
+
+#Enpoint 5
+@app.get("/sentiment_analysis/{year}")
+def sentiment_analysis(year : int):
+    df_endpoint5 = pd.read_csv("df_endpoint5.csv")
+    sentiment_data = df_endpoint5[df_endpoint5["Year"] == year]
+    sentiment_data_dict = sentiment_data.to_dict(orient="records")
+
+    return sentiment_data_dict
+
+
+#Endpoint Recommendation system
+@app.get("/Recommendation_system/{Game}")
+def recomendacion_juego(Game : str):   
+    df_RS = pd.read_csv("sistema_recomendacion.csv")
+    RS_data = df_RS[df_RS["Game"] == Game]
+    RS_data_dict = RS_data.to_dict(orient="records")
+
+    return RS_data_dict
